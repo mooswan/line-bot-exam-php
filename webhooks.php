b77d9ab8a716d34f09c3a07a82c71be838862401
mooswan <49270165+mooswan@users.noreply.github.com>
2019-04-04 15:43:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'PcCcSJBNuGNP3V8DK4XSsrLGap+/qtPPyAz65vuoA7jAkvidSQQiKXHOT+X5+85YZ+OWuX7j60kwjYaL/Jxzqjk/BSQhx1ixyaQ1hE8u58r4cz8ghQRwPZy+6tWX8rUgAaPk6uCLoqY1FFcpZNPcCQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
